0845a4c4f88255fc1836962d3f882adf5f73df7e
malijing <malijing@xinye.com>
2025-07-20 23:24:55 +0800

    0720搭建api-passenger骨架

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,12 @@
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// 这个注解说明是启动类
+public class ApiPassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
